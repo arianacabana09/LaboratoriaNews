419faa3587342c683ef3f6c4e92b0bf465cce0c9
Ariana Cabana <acabana.0904@gmail.com>
2017-07-07 20:38:55 -0500

    barra superior

--- public/assets/js/bundle.js
+++ public/assets/js/bundle.js
@@ -46,10 +46,9 @@ $(function (_) {
 });
 
 /*HOME*/
-// const btnSearch = $('');
 var Home = function (update) {
   var header = $("<header></header>");
-  var navContainer = $("<nav class=\"nav-extended\"></nav>");
+  var navMobile = $("<nav id=\"navMobile\" class=\"nav-extended\"></nav>");
   var divWrapper = $("<div class=\"nav-wrapper\"></div>");
   var btnLeft = $("<ul class=\"hide-on-med-and-down\"><li><a href=\"#\"><img src=\"assets/img/menu.png\">section</a></li><li><a href=\"#\"><img src=\"assets/img/search.png\">search</a></li></ul>");
   var optionsdes = $("<ul class=\"right hide-on-med-and-down\"><li><a href=\"https://www.facebook.com/laboratoria.la/\" target=\"_blank\"><img src=\"assets/img/fb.png\"></a></li><li><a href=\"https://twitter.com/LaboratoriaLA\" target=\"_blank\"><img src=\"assets/img/tw.png\"></a></li><li><a href=\"https://www.linkedin.com/company-beta/3718820/\" target=\"_blank\"><img src=\"assets/img/in.png\"></a></li></ul>");
@@ -58,14 +57,26 @@ var Home = function (update) {
   var menuBar = $("<a href=\"#\" data-activates=\"mobile-demo\" class=\"button-collapse\"><img src=\"assets/img/menu.png\"></a>");
   // const itemsMenu = $('<ul class="side-nav" id="mobile-demo"><li><a href="sass.html">Opinión</a></li><li><a href="sass.html">Cultura</a></li><li><a href="sass.html">Tecología</a></li><li><a href="sass.html">Perú</a></li></li><li><a href="sass.html">Mundo</a></li><li><a href="#">Economía</a><li><a>Lifestyle</a><li><a>Deporte<a></li></ul>');
 
+  var main = $("<div class=\"center hide-on-med-and-down\"><div><img src=\"assets/img/logoicon.png\"></div><span>Lunes, Junio 12 de 2017 | <img src=\"assets/img/cloud.png\"></span></div>");
+
+  var navDesktop = $("<nav id=\"navDesktop\"></nav>");
+  var divContainer = $("<div class=\"nav-wrapper\"></div>");
+  var divOptions = $("<div class=\"col s12 l12\"><a href=\"#\" class=\"breadcrumb\">Lo último</a><a href=\"#\" class=\"breadcrumb\">Opinión</a><a href=\"#!\" class=\"breadcrumb\">Cultura</a><a href=\"#\" class=\"breadcrumb\">Perú</a><a href=\"#\" class=\"breadcrumb\">Tecología</a><a href=\"#!\" class=\"breadcrumb\">Mundo</a><a href=\"#\" class=\"breadcrumb\">Economía</a><a href=\"#\" class=\"breadcrumb\">Lifestyle</a><a href=\"#!\" class=\"breadcrumb\">Deporte</a></div>");
+
   $("#root").append(header);
-  header.append(navContainer);
-  navContainer.append(divWrapper);
+  header.append(navMobile);
+  header.append(main);
+
+  navMobile.append(divWrapper);
   divWrapper.append(btnLeft);
   divWrapper.append(optionsdes);
 
   divWrapper.append(logo);
   divWrapper.append(menuBar);
+
+  main.append(navDesktop);
+  navDesktop.append(divContainer);
+  divContainer.append(divOptions);
   // divWrapper.append(itemsMenu);
   // return sliderIntro;
 };
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -42,10 +42,9 @@ $(_ =>{
 });
 
 /*HOME*/
-// const btnSearch = $('');
 const Home = (update) =>{
   const header = $('<header></header>');
-  const navContainer = $('<nav class="nav-extended"></nav>');
+  const navMobile = $('<nav id="navMobile" class="nav-extended"></nav>');
     const divWrapper = $('<div class="nav-wrapper"></div>');
       const btnLeft = $('<ul class="hide-on-med-and-down"><li><a href="#"><img src="assets/img/menu.png">section</a></li><li><a href="#"><img src="assets/img/search.png">search</a></li></ul>');
       const optionsdes = $('<ul class="right hide-on-med-and-down"><li><a href="https://www.facebook.com/laboratoria.la/" target="_blank"><img src="assets/img/fb.png"></a></li><li><a href="https://twitter.com/LaboratoriaLA" target="_blank"><img src="assets/img/tw.png"></a></li><li><a href="https://www.linkedin.com/company-beta/3718820/" target="_blank"><img src="assets/img/in.png"></a></li></ul>');
@@ -54,14 +53,26 @@ const Home = (update) =>{
       const menuBar = $('<a href="#" data-activates="mobile-demo" class="button-collapse"><img src="assets/img/menu.png"></a>')
       // const itemsMenu = $('<ul class="side-nav" id="mobile-demo"><li><a href="sass.html">Opinión</a></li><li><a href="sass.html">Cultura</a></li><li><a href="sass.html">Tecología</a></li><li><a href="sass.html">Perú</a></li></li><li><a href="sass.html">Mundo</a></li><li><a href="#">Economía</a><li><a>Lifestyle</a><li><a>Deporte<a></li></ul>');
 
+  const main = $('<div class="center hide-on-med-and-down"><div><img src="assets/img/logoicon.png"></div><span>Lunes, Junio 12 de 2017 | <img src="assets/img/cloud.png"></span></div>');
+
+    const navDesktop = $('<nav id="navDesktop"></nav>');
+      const divContainer = $('<div class="nav-wrapper"></div>');
+        const divOptions = $('<div class="col s12 l12"><a href="#" class="breadcrumb">Lo último</a><a href="#" class="breadcrumb">Opinión</a><a href="#!" class="breadcrumb">Cultura</a><a href="#" class="breadcrumb">Perú</a><a href="#" class="breadcrumb">Tecología</a><a href="#!" class="breadcrumb">Mundo</a><a href="#" class="breadcrumb">Economía</a><a href="#" class="breadcrumb">Lifestyle</a><a href="#!" class="breadcrumb">Deporte</a></div>')
+
   $('#root').append(header);
-  header.append(navContainer);
-  navContainer.append(divWrapper);
+  header.append(navMobile);
+  header.append(main);
+
+  navMobile.append(divWrapper);
   divWrapper.append(btnLeft);
   divWrapper.append(optionsdes);
 
   divWrapper.append(logo);
   divWrapper.append(menuBar);
+
+  main.append(navDesktop);
+  navDesktop.append(divContainer);
+  divContainer.append(divOptions);
   // divWrapper.append(itemsMenu);
   // return sliderIntro;
 }
